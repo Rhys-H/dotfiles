f921b0a5df55cc65fbcc522ad1ed9454b473f1db
Rhys Howells <rhys.howells01@gmail.com>
2020-05-23 14:10:00 +0100

    Add steps to brew setup to add to PATH

--- scripts/install_apps.sh
+++ scripts/install_apps.sh
@@ -95,6 +95,10 @@ if [[ $yes == y* ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
     echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.zprofile
     eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
     brew install gcc
     export LDFLAGS="-L/home/linuxbrew/.linuxbrew/opt/isl@0.18/lib"
     export CPPFLAGS="-I/home/linuxbrew/.linuxbrew/opt/isl@0.18/include"
