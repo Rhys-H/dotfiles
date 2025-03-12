6b8be1643578b18650eb3908f39a577d4cd0501a
Rhys-H <rhys.howells01@gmail.com>
2025-03-12 17:37:51 +0000

    nvim config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
-for file in $( ls -A | grep -vE 'bootstrap.sh|\.vscode|\.exclude*|\.git$|\.gitignore|.*.md' ) ; do
+for file in $( ls -A | grep -vE 'bootstrap.sh|\.exclude*|\.git$|\.gitignore|\.DS_Store|.*.md' ) ; do
+    if [ "$file" == ".config" ]; then
+        for config_file in $( ls -A .config | grep -vE '\.DS_Store' ); do
+            ln -sfv "$PWD/.config/$config_file" "$HOME/.config/$config_file"
+        done
+    else
         ln -sfv "$PWD/$file" "$HOME"
+    fi
 done
 echo "Symlinking complete"
-
