ada858f33aa2159f003dda0090f11d0d32653fad
Rhys-H <rhys.howells01@gmail.com>
2025-03-21 10:44:47 +0000

    Fix syntax colors in MacOS terminal
    And add IPython shell alias when debugging with pdb

--- .pdbrc.py
+++ .pdbrc.py
@@ -2,3 +2,7 @@ import pdb
 
 class Config(pdb.DefaultConfig):
     sticky_by_default = True
+    use_terminal256formatter = False
+
+    def setup(self, pdb):
+        print("Use 'emb' to enter IPython shell")
